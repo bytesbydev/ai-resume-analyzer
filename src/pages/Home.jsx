2ac4d531d0e8ed56e1df3b517f864901dd9ddb2e
devendrarawat-code <devendrarawat9876@gmail.com>
2026-04-17 21:16:31 +0530

    adding the upload section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,51 @@
-import React from 'react'
-import './Home.css'
-import Header from '../componets/Header'
+import React from "react";
+import "./Home.css";
+import Header from "../componets/Header";
 
 const Home = () => {
   return (
     <div>
-        <Header/> 
-        <div className="outor_div">
+      <Header />
+      <div className="outor_div">
         <div className="center_div">
-          <h3>Analyze your Resume</h3> <br />
-          Get actionable feedback to improve your resume and increase your chance with recruitors and ATS systems
+          <div className="center-heading-section">
+          <div className="center-heading">Analyze your Resume</div>
+          <div className="center-subheading">
+            Get actionable feedback to improve your resume and increase your
+            chance with recruitors and ATS systems
+          </div>
+          </div>
+          <div className="uploading-section">
+            <div>
+              <div className="upload-icon"></div>
+              <div className="upload-heading">Upload your resume</div>
+              <div className="upload-subheading"> Drag and drop or click to browse</div>  <br />
+              <input type="file" name="resume" />
+              <div>
+                <ul>
+                  <li>Pdf format only</li>
+                  <li>Max file size: 5MB</li>
+                  <li>Analysis takes 2 to 5 seconds</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div>icon</div>
+              <span>What we analyze</span>
+              <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta id tempore impedit sed architecto voluptate provident minus. Accusamus architecto, consequatur ea voluptate accusantium non, rerum hic ducimus in vitae temporibus?</span>
+            </div>
+            <div>
+              <div>icon</div>
+              <span>your privacy</span>
+              <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolore repudiandae numquam voluptas similique. Vitae, quos culpa! Fugiat nostrum placeat iste suscipit ratione molestias eum neque, asperiores architecto ad obcaecati.</span>
+            </div>
+          </div>
         </div>
-         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
