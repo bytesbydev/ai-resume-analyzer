a08b2aece6bc8517bc0e5a694c203c1132cdc9f9
devendrarawat-code <devendrarawat9876@gmail.com>
2026-05-05 20:49:26 +0530

    added some content to the css file

--- src/components/Home/UploadBox.jsx
+++ src/components/Home/UploadBox.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import "./UploadBox.css"
 const UploadBox = () => {
   return (
     <div>
@@ -11,9 +11,10 @@ const UploadBox = () => {
           <input type="file" />
           <div>
             <ul>
-              <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum ipsam, sapiente suscipit quos non autem libero impedit distinctio! Eius unde rerum impedit sapiente qui porro beatae sint dignissimos quos quae?</li>
-              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, obcaecati officiis expedita assumenda non nulla omnis eligendi temporibus vitae voluptatum magnam modi iusto, maiores consequuntur fugiat cupiditate? Ea, repellendus rem?</li>
-              <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vitae, iure nam dolor enim ipsam est deleniti nemo, ut at delectus repellendus inventore voluptates sunt ipsum dicta, corrupti exercitationem veritatis.</li>
+              <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum ipsam, sapiente </li>
+              <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum ipsam, sapiente </li>
+              <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum ipsam, sapiente </li>
+             
             </ul>
           </div>
         </div>
