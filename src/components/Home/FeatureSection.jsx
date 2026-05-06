8f3131a0a785a01bb867131685e18b92b71b3550
devendrarawat-code <devendrarawat9876@gmail.com>
2026-05-06 16:29:47 +0530

    added the readme.md file

--- src/components/Home/FeatureSection.jsx
+++ src/components/Home/FeatureSection.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import FeaturesCard from './FeaturesCard'
 import "./FeatureSection.css"
+import { ReactComponent as AiIcon } from "../../assets/image1.png";
+import { ReactComponent as AtsIcon } from "../../assets/image2.png";
 
 const FeatureSection = () => {
   const features=[{
-    icon:""
     heading:"What we analyze",
-    description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum ipsam, sapiente suscipit quos non autem libero impedit distinctio! Eius unde rerum impedit sapiente qui porro beatae sint dignissimos quos quae?"
+    description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum ipsam, sapiente suscipit quos non autem libero impedit distinctio! Eius unde rerum impedit sapiente qui porro beatae sint dignissimos quos quae?",
+    icon:AiIcon
   },{
      heading:"What we analyze",
-    description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum ipsam, sapiente suscipit quos non autem libero impedit distinctio! Eius unde rerum impedit sapiente qui porro beatae sint dignissimos quos quae?" 
+    description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum ipsam, sapiente suscipit quos non autem libero impedit distinctio! Eius unde rerum impedit sapiente qui porro beatae sint dignissimos quos quae?",
+    icon:AtsIcon
   }]
   return (
     <div>
