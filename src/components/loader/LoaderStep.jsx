fad53af32df024c1a1803947a5312078b8ae54e3
devendrarawat-code <devendrarawat9876@gmail.com>
2026-05-13 15:08:51 +0530

    edited the design flaws

--- src/components/loader/LoaderStep.jsx
+++ src/components/loader/LoaderStep.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import "./Loaderstep.css"
-const LoaderStep = () => {
+const LoaderStep = ({icon,title,time}) => {
   return (
     <div className='loader-section'>
       <div className='loader-cards'>
-        <div className="icon-description"> <div className='icon'>icon</div></div>
-        <div className='description-section'> <span className='l-subdescription'>Parsing resume content</span> <br />
-        <span> 1-2s</span></div>
+        <div className="icon-description"> <div className='icon'>{icon}</div></div>
+        <div className='description-section'> <span className='l-subdescription'>{title}</span> <br />
+        <span> {time}</span></div>
       </div>
-      <div className='loader-cards'>
+      {/* <div className='loader-cards'>
           <div className="icon-description"> <div className='icon'>icon</div></div>
         <div className='description-section'> <span className='l-subdescription'>Analyzing skills and experience</span> <br />
         <span> 1-2s</span></div>
@@ -17,7 +17,7 @@ const LoaderStep = () => {
          <div className="icon-description"> <div className='icon'>icon</div></div>
         <div className='description-section'> <span className='l-subdescription'>Generating insights</span>
         <span> 1-2s</span></div>
-      </div>
+      </div> */}
     </div>
   )
 }
