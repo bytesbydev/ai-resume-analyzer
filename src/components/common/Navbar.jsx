2eddfd1274ac2554e2a434aa910ac2ba6e613521
devendrarawat-code <devendrarawat9876@gmail.com>
2026-05-15 16:08:25 +0530

    removed some of the css files

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-import "./Navbar.css"
+
 const Navbar = () => {
   return (
     <div>
-        <div className=' w-full text-[red] border-b-2 border-b-[rgb(183,176,176)] border-solid'>
-          <div className='flex w-[30%] gap-2.5 ml-[39px] p-[5px]'>
-            <div className="icon">Icon</div>
-            <div className='text-[black]'>
-              ScanStack <br /> Resume analysis
-            </div>
+      <div className="w-full text-red-500 border-b-2 border-gray-300">
+        
+        <div className="flex w-[30%] gap-2.5 ml-9.75 p-1.25">
+          
+          <div className="icon">
+            Icon
           </div>
+
+          <div className="text-black">
+            ScanStack <br />
+            Resume analysis
+          </div>
+
         </div>
+
+      </div>
     </div>
   );
 };
--- src/components/common/ProgressBar.jsx
+++ src/components/common/ProgressBar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./ProgressBar.css";
 
 const ProgressBar = ({ progress }) => {
   return (
