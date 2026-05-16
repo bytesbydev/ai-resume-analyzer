1a372ba0cdbf1a5e28006e3a38cc8c65affa803c
devendrarawat-code <devendrarawat9876@gmail.com>
2026-05-16 16:19:29 +0530

    added some code into the results page

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -5,7 +5,14 @@ const Results = () => {
   return (
     <div>
       <div>
+        <div className='result-header'>
         <ResultHeader/>
+        </div>
+        <div  className='w-[90%] h-full border-2 border-black solid'>
+          <span className='font-black text-2xl'>Resume Analysis report</span> <br />
+          <span className='text-md text-gray-600'>Comprehensive breakdown with actionable recommendations</span>
+        </div>
+        <div className='ats score section'></div>
       </div>
     </div>
   )
