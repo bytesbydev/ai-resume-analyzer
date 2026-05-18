d4d5056c385101576dcb82fbf082872d9f5049c2
devendrarawat-code <devendrarawat9876@gmail.com>
2026-05-18 16:24:50 +0530

    made some files and code in the results,jsx

--- src/components/results/ScoreSection.jsx
+++ src/components/results/ScoreSection.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React from "react";
 import { Award, TrendingUp, AlertCircle, CheckCircle2 } from "lucide-react";
 const ScoreSection = () => {
+  const score = 88;
   return (
     <div>
-      <div className='border-2 border-black solid mt-11 '>
-        <div  className="w-5 h-5"><Award className='bg-blue-700 text-white' /></div>
-        <span>Overall Quality Score</span>
-        <span>Based on the content, formatting and ATS compatibility</span>
+      <div className="flex items-center gap-3 mb-6">
+        <div className="w-10 h-10 rounded-lg bg-blue-600 flex items-center justify-center">
+          <Award className="w-5 h-5 text-white" />
+        </div>
+        <div>
+          <h3 className="text-lg font-semibold text-gray-900">
+            Overall Quality Score
+          </h3>
+          <p className="text-sm text-gray-500">
+            Based on content, formatting, and ATS compatibility
+          </p>
+        </div>
+      </div>
+      <div className="ats-score">
+        <span>Resume score </span>
+        <div>
+          <span
+            className={`text-5xl font-semibold ${
+              score >= 80
+                ? "text-green-600"
+                : score >= 60
+                  ? "text-amber-600"
+                  : "text-red-600"
+            }`}
+          >
+            {score}
+          </span>
+          <span className="text-2xl font-medium text-gray-700">/100</span>
+        </div>
+        <div className="h-3 w-full rounded border-gray-300"></div> 
+        <div className={w-${score}>
+
+       5 </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ScoreSection
+export default ScoreSection;
--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -12,10 +12,10 @@ const Results = () => {
         <main  className='flex flex-col items-center'>
           <div className='w-[90%] h-fit border-2 border-black solid mt-7'>
         <div>
-          <span className='font-black text-2xl'>Resume Analysis report</span> <br />
-          <span className='text-md text-gray-600'>Comprehensive breakdown with actionable recommendations</span>
+          <span className=' font-semibold text-2xl'>Resume Analysis report</span> <br />
+          <span className='text-md font-semibold text-gray-600'>Comprehensive breakdown with actionable recommendations</span>
         </div>
-        <div className='ats score section p-5'>
+        <div className='ats score section p-5 border-2 border-gray-300 solid mt-5'>
           <ScoreSection/>
         </div>
           </div>
