b3bbd64f4b47ecc5d3cc35e69885ec5d388cb861
devendrarawat-code <devendrarawat9876@gmail.com>
2026-05-29 23:00:19 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Home from './pages/Home'
-import Analyzing from './pages/Analyzing'
-import Results from './pages/Results'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Analyzing from "./pages/Analyzing";
+import Results from "./pages/Results";
 
 const App = () => {
   return (
-    <div>
-    {/* <Home/> */}
-    {/* <Analyzing/> */}
-      <Results/>
-      </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/analyzing" element={<Analyzing />} />
+      <Route path="/results" element={<Results />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
