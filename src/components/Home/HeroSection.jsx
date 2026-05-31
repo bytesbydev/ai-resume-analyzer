6cce7ef8ad96256c5c3fbf0fc1fc732b6ce95e68
devendrarawat-code <devendrarawat9876@gmail.com>
2026-05-31 18:00:00 +0530

    Updated the herosection

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 const HeroSection = () => {
   return (
-    <div className='mt-17.5 font-serif'>
-        
-        <span className='text-[40px]'>Analyze your Resume </span> <br />
-        <div className='text-gray-500 text-[19px] mt-5'>Get actionable feedback to improve your resume and increase your chances with the recruiters and ATS system.</div>
-    </div>
+    <div className="mt-10">
+          <h2 className="text-3xl font-semibold text-gray-900 mb-3">
+            Analyze Your Resume
+          </h2>
+          <p className="text-lg text-gray-600">
+            Get actionable feedback to improve your resume and increase your chances with recruiters and ATS systems.
+          </p>
+        </div>
   )
 }
 
