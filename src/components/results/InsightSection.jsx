5755945aa33c994ad130e09977f204d75249280e
devendrarawat-code <devendrarawat9876@gmail.com>
2026-06-02 21:31:39 +0530

    enhanced the code and logic

--- src/components/results/InsightSection.jsx
+++ src/components/results/InsightSection.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import { CheckCircle2 } from "lucide-react";
-import InsightCard from './InsightCard';
-const InsightSection = () => {
+import React from "react";
+import InsightCard from "./InsightCard";
 
+const InsightSection = ({ result }) => {
   return (
-    <div className=' flex gap-6 solid h-fit p-3 w-full mt-10'>
-      <InsightCard type="strength" title="Strengths"/>
-      <InsightCard type="improvement" title="Improvements"/>
-      <InsightCard type="critical" title="Critical Issues"/>
-    </div>
-  )
-}
+    <div className="flex gap-6 h-fit p-3 w-full mt-10">
+      <InsightCard
+        title="Strengths"
+        type="strength"
+        data={result.strengths}
+      />
 
-export default InsightSection
+      <InsightCard
+        title="Improvements"
+        type="improvement"
+        data={result.improvements}
+      />
+
+      <InsightCard
+        title="Critical Issues"
+        type="critical"
+        data={result.criticalIssues}
+      />
+    </div>
+  );
+};
+
+export default InsightSection;
