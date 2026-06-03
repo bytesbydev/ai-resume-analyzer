3ed770035c846338e5803805e9a33831fe8b809b
devendrarawat-code <devendrarawat9876@gmail.com>
2026-06-03 21:33:08 +0530

    changed the ui to fit for the mobile devices

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,16 @@ import Navbar from "../components/common/Navbar";
 import HeroSection from "../components/Home/HeroSection";
 import UploadBox from "../components/Home/UploadBox";
 import FeatureSection from "../components/Home/FeatureSection";
+
 const Home = () => {
   return (
-    <div className="home-container">
+    <div className="home-container w-full">
       <Navbar />
 
-      <div className="h-fit w-full flex justify-center pb-12.5">
-        <main className="h-fit w-6/12">
+      <div className="w-full flex justify-center px-4 sm:px-6 lg:px-8 pb-12">
+        <main className="w-full max-w-3xl lg:max-w-4xl">
           <HeroSection />
-
           <UploadBox />
-
           <FeatureSection />
         </main>
       </div>
