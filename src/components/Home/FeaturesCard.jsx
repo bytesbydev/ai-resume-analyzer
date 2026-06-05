0d345f5cc79f24eef00e9604af1e7121f3b8cb65
devendrarawat-code <devendrarawat9876@gmail.com>
2026-06-05 20:08:09 +0530

    made it fit for the mobile view

--- src/components/Home/FeaturesCard.jsx
+++ src/components/Home/FeaturesCard.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 const FeaturesCard = ({icon,title, description }) => {
   return (
-    <div className='gap-5 h-full w-full flex bg-white p-3.75'>
-   <img src={icon} alt={title} className=' h-10 w-10'/>
-    <div>
-      <span className=' text-xl'>{title}</span> 
-      <div className='text-[15px] mt-2.5'>{description}</div>
-    </div>
+     <div className="flex gap-4 bg-white p-4 sm:p-5 rounded-lg shadow-sm w-full">
+
+      <img
+        src={icon}
+        alt={title}
+        className="h-8 w-8 sm:h-10 sm:w-10 shrink-0"
+      />
+
+      <div>
+        <span className="text-base sm:text-lg font-medium">
+          {title}
+        </span>
+
+        <div className="text-sm sm:text-[15px] mt-2 text-gray-600">
+          {description}
+        </div>
+      </div>
     </div>
   );
 };
