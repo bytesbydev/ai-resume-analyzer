c61fa4098284aef7900bc5f4252f80256312c2aa
devendrarawat-code <devendrarawat9876@gmail.com>
2026-06-08 20:52:44 +0530

    made some changes in the layout

--- src/components/results/StatsCards.jsx
+++ src/components/results/StatsCards.jsx
@@ -51,7 +51,7 @@ const cards = [
 
   return (
 
-    <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
 
       {cards.map((card, index) => (
 
